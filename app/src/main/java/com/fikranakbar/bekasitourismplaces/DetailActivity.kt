56a05cb7dbbf768dc4b1ec49c5d548f95ae414d6
Fikran Akbar <fikranakbar756@gmail.com>
2021-07-25 21:02:21 +0700

    fixing and polishing detail activity

--- app/src/main/java/com/fikranakbar/bekasitourismplaces/DetailActivity.kt
+++ app/src/main/java/com/fikranakbar/bekasitourismplaces/DetailActivity.kt
@@ -2,6 +2,8 @@ package com.fikranakbar.bekasitourismplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.fikranakbar.bekasitourismplaces.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -11,10 +13,37 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityDetailBinding
-    
+    private lateinit var tourismPlace: TourismPlace
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        intent.getParcelableExtra<TourismPlace>(TOURISM_PLACE_DATA)?.also { tourismPlace = it }
+
+        setBackButton()
+        setDetailInformation()
+    }
+
+    private fun setDetailInformation() {
+        binding.apply {
+            tvDescription.text = tourismPlace.desc
+            tvTitlePlace.text = tourismPlace.name
+            tvLocationContent.text = tourismPlace.location
+            tvOperationHoursContent.text = tourismPlace.operationHour
+            tvTicketPricesContent.text = tourismPlace.ticketPrice
+
+            Glide.with(this@DetailActivity)
+                .load(tourismPlace.imageHeader)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(ivHeader)
+        }
+    }
+
+    private fun setBackButton() {
+        binding.cvBackButton.setOnClickListener {
+            finish()
+        }
     }
 }
